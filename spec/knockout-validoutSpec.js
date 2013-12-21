0e40ac0663bf418023e6cf489b783029570cbb23
Evgeni Kunev <evgeni@kunev.me>
2013-12-21 17:27:04 +0200

    Add tests for integer type validator

--- spec/knockout-validoutSpec.js
+++ spec/knockout-validoutSpec.js
@@ -1,4 +1,10 @@
-/*globals describe: true, it: true, expect: true, ko: true*/
+/*globals
+  describe: true,
+  it: true,
+  expect: true,
+  ko: true,
+  beforeEach: true
+*/
 
 (function () {
   var model = {};
@@ -11,6 +17,21 @@
     it('approves valid integers', function () {
       model.integer_property('5');
       expect(model.integer_property.is_valid()).toBe(true);
+    });
+
+    it('does not approve undefined values', function () {
+      model.integer_property(undefined);
+      expect(model.integer_property.is_valid()).toBe(false);
+    });
+
+    it('does not approve complex objects', function () {
+      model.integer_property({not: 'a number'});
+      expect(model.integer_property.is_valid()).toBe(false);
+    });
+
+    it('does not approve NaN', function () {
+      model.integer_property(NaN);
+      expect(model.integer_property.is_valid()).toBe(false);
     });
   });
 }());
