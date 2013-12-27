421b2759ea0dc18c74281070eabafda21734989c
Evgeni Kunev <evgeni@kunev.me>
2013-12-27 17:59:16 +0200

    Add tests for gt and gte validators

--- spec/knockout-validoutSpec.js
+++ spec/knockout-validoutSpec.js
@@ -17,6 +17,14 @@ if (typeof exports !== "undefined" && exports !== null) {
     type: 'integer'
   });
 
+  model.greater_than_five = ko.observable().extend({
+    gt: 5
+  });
+
+  model.greater_than_or_equal_to_five = ko.observable().extend({
+    gte: 5
+  });
+
   describe('integer validator', function () {
     it('approves valid integers', function () {
       model.integer_property('5');
@@ -36,6 +44,40 @@ if (typeof exports !== "undefined" && exports !== null) {
     it('does not approve NaN', function () {
       model.integer_property(NaN);
       expect(model.integer_property.is_valid()).toBe(false);
+    });
+  });
+
+  describe('greater than validator', function () {
+    it('approves of number grater than the argument', function () {
+      model.greater_than_five(12);
+      expect(model.greater_than_five.is_valid()).toBe(true);
+    });
+
+    it('does not approve numbers less than the argument', function () {
+      model.greater_than_five(3);
+      expect(model.greater_than_five.is_valid()).toBe(false);
+    });
+
+    it('does not approve an equal value', function () {
+      model.greater_than_five(5);
+      expect(model.greater_than_five.is_valid()).toBe(false);
+    });
+  });
+
+  describe('greater than or equal validator', function () {
+    it('approves of number grater than the argument', function () {
+      model.greater_than_or_equal_to_five(12);
+      expect(model.greater_than_or_equal_to_five.is_valid()).toBe(true);
+    });
+
+    it('does not approve numbers less than the argument', function () {
+      model.greater_than_or_equal_to_five(3);
+      expect(model.greater_than_or_equal_to_five.is_valid()).toBe(false);
+    });
+
+    it('approves an equal value', function () {
+      model.greater_than_or_equal_to_five(5);
+      expect(model.greater_than_or_equal_to_five.is_valid()).toBe(true);
     });
   });
 }());
