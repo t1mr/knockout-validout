f80e394f39547f0c3425853fb908ce642ed5beb7
Evgeni Kunev <evgeni@kunev.me>
2013-12-27 15:30:29 +0200

    Add grunt task to run tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,4 +14,19 @@ module.exports = function(grunt) {
   });
 
   grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.registerTask('test', 'run test in browser', function() {
+    grunt.util.spawn({
+      cmd: process.argv[0],
+      args: ['node_modules/jasmine-node/lib/jasmine-node/cli.js', '--color' ,'spec/knockout-validoutSpec.js'],
+      opts: { stdio: 'inherit' }
+    }, function (error, result) {
+      if (error) {
+        console.error('Error: ' + error);
+        console.error('Return code: ' + result.code);
+      }
+      console.log(result.stdout.toString());
+      return;
+    });
+  });
 };
