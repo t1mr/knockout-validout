813dae64291c7abd86ded478101dac5d8d32f96b
vdimchev <vlado@blue-edge.bg>
2014-01-06 18:54:05 +0200

    Grunt test task extended with second argument to run single spec file only.
    Invoke with:
    grunt test::some
    to run the test in spec/knockout-validout-some-spec.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,10 +17,11 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-uglify');
 
-  grunt.registerTask('test', 'run test in browser', function(type) {
+  grunt.registerTask('test', 'run test in browser', function(type, spec) {
+    var target = spec === undefined ? 'spec' : ('spec/knockout-validout-' + spec + '-spec.js');
     var console_test = grunt.util.spawn.bind(grunt.util, {
       cmd: process.argv[0],
-      args: ['node_modules/jasmine-node/lib/jasmine-node/cli.js', '--color', '--matchall', '--verbose' ,'spec'],
+      args: ['node_modules/jasmine-node/lib/jasmine-node/cli.js', '--color', '--matchall', '--verbose' , target],
       opts: { stdio: 'inherit' }
     }, function (error, result) {
       if (error) {
